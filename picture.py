cc96122e9d57449a754d2bc339fc0955b6c4459f
inagetsutomu <y.nabe.1008@gmail.com>
2025-06-16 23:55:27 +0900

    picture編集、SampleRotateObliqueの余分な部分削除

--- picture.py
+++ picture.py
@@ -20,15 +20,14 @@ def make_dir(dir=None):
     
 
 # gif保存
-def save_gif(dir, flame=1000,loop=0):
-    image_folder = dir + "/images"
+def save_gif(dir, image_dir, flame=1000,loop=0):
     output_dir = os.path.join(dir, "animation.gif")
     # 拡張子が .png または .tif の画像を読み込む（昇順でソート）
-    image_files = sorted([f for f in os.listdir(image_folder) if f.endswith(('.png', '.tif', '.jpg'))],
+    image_files = sorted([f for f in os.listdir(image_dir) if f.endswith(('.png', '.tif', '.jpg'))],
                          key=extract_number)
 
     # フルパスに変換して画像を読み込み
-    images = [Image.open(os.path.join(image_folder, f)) for f in image_files]
+    images = [Image.open(os.path.join(image_dir, f)) for f in image_files]
 
     # GIFとして保存（ループあり、各フレーム200ms）
     images[0].save(output_dir,
@@ -44,8 +43,8 @@ def extract_number(filename):
     return int(match.group()) if match else -1
 
 # 2次元配列を画像で保存する関数
-def save(data, path, title):
-    data=Image.fromarray(data)
+def save(image, path, title):
+    data=Image.fromarray(image)
     data.save("./"+ path + "/" + title)
 
 if __name__ == "__main__":
